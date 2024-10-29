75919fef373ccfa20e2854ea66f33b5c763e2520
Azzazelloqq <bog22232@ya.ru>
2024-10-29 12:45:27 +0400

    change config container and parse

--- Assets/Code/Config/Config.cs
+++ Assets/Code/Config/Config.cs
@@ -1,30 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Code.Config
 {
 public class Config : IConfig
 {
+	public bool IsInitialized { get; private set; }
+	
+	private Dictionary<Type, IConfigData> _configDataContainer;
 	private readonly IConfigParser _configParser;
 
-	private readonly IConfigData[] _configDataContainer;
-	
 	public Config(IConfigParser configParser)
 	{
 		_configParser = configParser;
-		_configDataContainer = _configParser.Parse();
+		
+		IsInitialized = false;
+	}
+
+	public void Initialize()
+	{
+		if (IsInitialized)
+		{
+			throw new Exception("Config is already initialized");
+		}
+		
+		_configDataContainer = ParseConfig();
+
+		IsInitialized = true;
+	}
+
+	public async Task InitializeAsync(CancellationToken token)
+	{
+		if (IsInitialized)
+		{
+			throw new Exception("Config is already initialized");
+		}
+
+		_configDataContainer = await ParseConfigAsync(token);
 	}
 	
 	public T GetData<T>() where T : IConfigData
 	{
-		foreach (var configData in _configDataContainer)
+		if (!_configDataContainer.TryGetValue(typeof(T), out var data))
 		{
-			if (configData is T concreteData)
-			{
-				return concreteData;
-			}
+			throw new Exception($"Need add data {typeof(T).Name} in {_configParser.GetType().Name}");
 		}
 
-		throw new Exception($"Need add data {typeof(T).Name} in {_configParser.GetType().Name}");
+		if (data is T concreteData)
+		{
+			return concreteData;
+		}
+
+		throw new Exception($"Config by type {typeof(T)} contains in container as different type {data.GetType()}");
+
+	}
+
+	private Dictionary<Type, IConfigData> ParseConfig()
+	{
+		var configData = _configParser.Parse();
+		var parsedData = new Dictionary<Type, IConfigData>(configData.Length);
+		
+		foreach (var data in configData)
+		{
+			var type = data.GetType();
+			parsedData[type] = data;
+		}
+
+		return parsedData;
+	}
+
+	private async Task<Dictionary<Type, IConfigData>> ParseConfigAsync(CancellationToken token)
+	{
+		var configData = await _configParser.ParseAsync(token);
+		var parsedData = new Dictionary<Type, IConfigData>(configData.Length);
+		
+		foreach (var data in configData)
+		{
+			var type = data.GetType();
+			parsedData[type] = data;
+		}
+
+		return parsedData;
+
 	}
 }
 }
--- Assets/Code/Config/IConfig.cs
+++ Assets/Code/Config/IConfig.cs
@@ -1,7 +1,14 @@
-﻿namespace Code.Config
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Code.Config
 {
 public interface IConfig
 {
+	public bool IsInitialized { get; }
+	
+	public void Initialize();
+	public Task InitializeAsync(CancellationToken token);
 	public T GetData<T>() where T : IConfigData;
 }
 }
