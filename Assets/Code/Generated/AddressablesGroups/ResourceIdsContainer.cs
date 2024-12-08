79bc51c070b579abc359d6183e2fd5588768baec
Azzazelloqq <bog22232@ya.ru>
2024-12-08 15:38:50 +0400

    add resource ids code generator

---
+++ Assets/Code/Generated/AddressablesGroups/ResourceIdsContainer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Code.Generated.Addressables;
+
+namespace Code.Generated.Addressables
+{
+    public static class ResourceIdsContainer
+    {
+        public static readonly DefaultLocalGroup DefaultLocalGroup = new DefaultLocalGroup();
+    }
+}
